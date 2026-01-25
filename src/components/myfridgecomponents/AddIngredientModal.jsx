b897864509e20f331b02d149906bb2034e0230ba
dev-lyj9703070 <lyj9703070@gmail.com>
2026-01-26 01:25:21 +0900

    나의냉장고 재료추가페이지

---
+++ src/components/myfridgecomponents/AddIngredientModal.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import S, { AddButton, Page, Wrapper } from '../../pages/myfridge/style';
+
+const AddIngredientModal = () => {
+
+
+
+  return (
+    <Page>
+      <Wrapper>
+        <div>
+          <img className='firdgeImg' src={`${process.env.PUBLIC_URL}/assets/images/fridge.png`} alt="냉장고" />
+        </div>
+        <div>
+          <AddButton>냉장고를 채워볼까요?</AddButton>
+        </div>
+      </Wrapper>
+    </Page>
+  );
+};
+
+export default AddIngredientModal;
